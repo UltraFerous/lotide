66abcd13e664294b3e5de7de822da279c948cf8d
UltraFerous <jandoga31@gmail.com>
2023-10-16 15:55:45 -0400

    Added head.js and removed return from assertEqual

--- head.js
+++ head.js
@@ -7,9 +7,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// FUNCTION IMPLEMENTATION
+const head = function(arr) {
+  return arr[0];
+};
+
 // TEST CODE
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+console.log(head([0]));
+console.log(head([]));
+
 
