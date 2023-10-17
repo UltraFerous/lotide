50a03ee5e6285592767898fa7d26905bb0b2e158
UltraFerous <jandoga31@gmail.com>
2023-10-17 16:38:21 -0400

    Fixed mistake in test cases

--- eqArrays.js
+++ eqArrays.js
@@ -36,6 +36,6 @@ const eqArrays = function(arrOne, arrTwo) {
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, '3'], [1, 2, 3]), false); // => should FAIL
-assertEqual(eqArrays([], []), true); // => should FAIL
+assertEqual(eqArrays([], []), true); // => should TRUE
 assertEqual(eqArrays([1], [1, 2]), false); // => should FAIL
-assertEqual(eqArrays(["Test"], ["Test"]), true); // => should FAIL
+assertEqual(eqArrays(["Test"], ["Test"]), true); // => should TRUE
