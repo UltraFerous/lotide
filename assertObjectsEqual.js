3d76f9aa968eb1b427eb11ddb45913c6e1633cf2
UltraFerous <jandoga31@gmail.com>
2023-10-19 14:38:31 -0400

    Added map

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -45,10 +45,10 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
 
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
     return;
   }
-  console.log(`🛑🛑🛑 Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+  console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 };
 
 
---
+++ map.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: [${arrOne}] === [${arrTwo}]`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: [${arrOne}] !== [${arrTwo}]`);
+};
+
+const map = function(array, callback) {
+  const results = [];
+
+  for(let index of array){
+    results.push(callback(index));
+  }
+  return results;
+}
+
+// TEST CODE
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+
+const actualResults = words.map(word => word[0]);
+assertArraysEqual(results1, actualResults)
