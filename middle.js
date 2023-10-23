db185d9786ee0286bf67bdd6bb953adc9e9515df
UltraFerous <jandoga31@gmail.com>
2023-10-23 13:48:53 -0400

    Modularized middle

--- middle.js
+++ middle.js
@@ -1,36 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(arrOne, arrTwo) {
-
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo)) {
-    console.log(`✅✅✅ Assertion Passed: [${arrOne}] === [${arrTwo}]`);
-    return;
-  }
-  console.log(`🛑🛑🛑 Assertion Failed: [${arrOne}] !== [${arrTwo}]`);
-};
-
-// FUNCTION IMPLEMENTATION
 const middle = function(arr) {
   const arrLength = arr.length;
   if (arrLength <= 2) {
@@ -42,11 +10,4 @@ const middle = function(arr) {
   }
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
+module.exports = middle;
