4c0ee37e3f24883eb010fc717660691d8739027e
UltraFerous <jandoga31@gmail.com>
2023-10-23 19:04:15 -0400

    Removed some unused functions.

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
