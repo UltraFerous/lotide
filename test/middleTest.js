2098ee29544d0af3820334dd33ba0e6cf3e5de9d
UltraFerous <jandoga31@gmail.com>
2023-10-23 14:26:25 -0400

    Refactored middleTest and tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,31 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns empty when ([1]) is passed", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns empty when ([1, 2]) is passed", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns 2 when ([1, 2, 3]) is passed", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2,3] when ([1, 2, 3, 4]) is passed", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3,4] when ([1, 2, 3, 4, 5, 6]) is passed", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns 2 when ([1, 2, 3, 4, 5]) is passed", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
+
 // TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,23 @@
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
+
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[0], "Lighthouse");
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[1], "Labs");
-assertEqual(tail([0])[0], undefined);
-assertEqual(tail([])[0], undefined);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns Lighthouse when ([Hello, Lighthouse, Labs])[0] is passed", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+  it("returns Labs when ([Hello, Lighthouse, Labs])[1] is passed", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+  it("returns undefined when head([0]) is passed", () => {
+    assert.strictEqual(tail([0])[0], undefined);
+  });
+  it("returns '[]' for []", () => {
+    assert.strictEqual(tail([])[0], undefined);
+  });
+  it("returns an array with a length of 2 when [Yo Yo, Lighthouse, Labs] is passed", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+});
